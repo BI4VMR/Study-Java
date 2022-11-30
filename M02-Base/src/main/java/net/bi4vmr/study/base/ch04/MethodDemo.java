c41d054f1f2f8c53d3f65ad45da1012ff9ff95ff
BI4VMR <bi4vmr@qq.com>
2022-12-01 00:33:54 +0800

    92af5390-f2b4-49e1-950e-8af07fe799ad

---
+++ M02-Base/src/main/java/net/bi4vmr/study/base/ch04/MethodDemo.java
@@ -0,0 +1,15 @@
+package net.bi4vmr.study.base.ch04;
+
+/**
+ * Name        : MethodDemo
+ * <p>
+ * Author      : BI4VMR
+ * <p>
+ * Email       : bi4vmr@qq.com
+ * <p>
+ * Date        : 2022-11-30 19:55
+ * <p>
+ * Description : TODO 添加描述
+ */
+public class MethodDemo {
+}
