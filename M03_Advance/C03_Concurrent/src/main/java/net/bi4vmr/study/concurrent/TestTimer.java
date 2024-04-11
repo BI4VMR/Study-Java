2a191cc2555a2704e42df7b5efb3dc37e268d468
BI4VMR <bi4vmr@outlook.com>
2024-04-11 17:21:10 +0100

    7E5A696F-DEBF-48F1-B46C-8AF626849B9B

--- M03_Advance/C03_Concurrent/src/main/java/net/bi4vmr/study/concurrent/TestTimer.java
+++ M03_Advance/C03_Concurrent/src/main/java/net/bi4vmr/study/concurrent/TestTimer.java
@@ -1,6 +1,8 @@
 package net.bi4vmr.study.concurrent;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,11 +35,68 @@ public class TestTimer {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                System.out.println("Time:" + LocalDateTime.now());
+                System.out.println("Time: " + LocalDateTime.now());
             }
         };
         Timer timer = new Timer();
-//        timer.schedule(timerTask,1000L,1000L);
-        timer.scheduleAtFixedRate(timerTask, 1000L, 1000L);
+        timer.schedule(timerTask, 1000L, 1000L);
+        timer.schedule(timerTask, 1000L, 1000L);
+        timer.schedule(timerTask, 1000L, 1000L);
+        // timer.schedule();
+        // timer.scheduleAtFixedRate(timerTask, 1000L, 1000L);
+    }
+
+    /*
+     * Timer
+     */
+    static void example02() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Time: " + LocalTime.now());
+            }
+        };
+
+        // 获取当前时间
+        Date date = new Date();
+        System.out.println("Reference Time: " + date);
+
+        // 阻塞当前线程3秒，不处理任何任务。
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 创建Timer并开启任务，参考时间点设置为3秒前。
+        Timer timer = new Timer();
+        timer.schedule(timerTask, date, 1000L);
+    }
+
+    /*
+     * Timer
+     */
+    static void example03() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Time: " + LocalTime.now());
+            }
+        };
+
+        // 获取当前时间
+        Date date = new Date();
+        System.out.println("Reference Time: " + date);
+
+        // 阻塞当前线程3秒，不处理任何任务。
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 创建Timer并开启任务，参考时间点设置为3秒前。
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, date, 1000L);
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+// Groovy写法
 // tasks.withType(JavaCompile) {
 //     options.encoding = "UTF-8"
 // }
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
