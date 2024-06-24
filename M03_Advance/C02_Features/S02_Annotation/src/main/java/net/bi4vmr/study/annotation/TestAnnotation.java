eada96e36e9a4994cd2a28f863c4dcc4fbfda2cf
BI4VMR <bi4vmr@outlook.com>
2024-06-24 23:45:18 +0800

    6216BE53-958B-46EF-B27C-F2A987995A6D

---
+++ M03_Advance/C02_Features/S02_Annotation/src/main/java/net/bi4vmr/study/annotation/TestAnnotation.java
@@ -0,0 +1,20 @@
+package net.bi4vmr.study.annotation;
+
+/**
+ * 测试代码：注解。
+ *
+ * @author BI4VMR
+ */
+public class TestAnnotation {
+
+    public static void main(String[] args) {
+        example01();
+    }
+
+    /*
+     * 示例：使用常量表示季节
+     */
+    static void example01() {
+
+    }
+}
