d68ce8af169f7e55e58bd421144f1cb1664f2144
BI4VMR <bi4vmr@outlook.com>
2024-06-25 18:11:34 +0800

    D140680D-A02F-4832-9D44-81F14883BFD7

---
+++ M01_Overview/C00_Temporary/src/main/java/FileType.java
@@ -0,0 +1,194 @@
+/**
+ * TODO 添加简述。
+ * <p>
+ * TODO 添加详情。
+ *
+ * @author BI4VMR
+ */
+//文件的字节码头
+
+public enum FileType {
+    /**
+     * JEPG.
+     */
+    JPEG("FFD8FF"),
+
+    /**
+     * PNG.
+     */
+    PNG("89504E47"),
+
+    /**
+     * Windows Bitmap.
+     */
+    BMP("424D"),
+
+    /**
+     * GIF.
+     */
+    GIF("47494638"),
+
+    /**
+     * TIFF.
+     */
+    TIFF("49492A00"),
+
+    /**
+     * CAD.
+     */
+    DWG("41433130"),
+
+    /**
+     * Adobe Photoshop.
+     */
+    PSD("38425053"),
+
+    /**
+     * Rich Text Format.
+     */
+    RTF("7B5C727466"),
+
+    /**
+     * XML.
+     */
+    XML("3C3F786D6C"),
+
+    /**
+     * HTML.
+     */
+    HTML("68746D6C3E"),
+
+    /**
+     * Email [thorough only].
+     */
+    EML("44656C69766572792D646174653A"),
+
+    /**
+     * Outlook Express.
+     */
+    DBX("CFAD12FEC5FD746F"),
+
+    /**
+     * Outlook (pst).
+     */
+    PST("2142444E"),
+
+    /**
+     * MS Word/Excel.
+     */
+    XLS_DOC("D0CF11E0"),
+
+    /**
+     * MS Access.
+     */
+    MDB("5374616E64617264204A"),
+
+    /**
+     * WordPerfect.
+     */
+    WPD("FF575043"),
+
+    /**
+     * Postscript.
+     */
+    EPS("252150532D41646F6265"),
+
+    /**
+     * Adobe Acrobat.
+     */
+    PDF("255044462D312E"),
+
+    /**
+     * Quicken.
+     */
+    QDF("AC9EBD8F"),
+
+    /**
+     * Windows Password.
+     */
+    PWL("E3828596"),
+
+    /**
+     * ZIP Archive.
+     */
+    ZIP("504B0304"),
+
+    /**
+     * RAR Archive.
+     */
+    RAR("52617221"),
+
+    /**
+     * Wave.
+     */
+    WAV("57415645"),
+
+    /**
+     * AVI.
+     */
+    AVI("41564920"),
+
+    /**
+     * Real Audio.
+     */
+    RAM("2E7261FD"),
+
+    /**
+     * Real Media.
+     */
+    RM("2E524D46"),
+
+    /**
+     * MPEG (mpg).
+     */
+    MPG("000001BA"),
+
+    /**
+     * Quicktime.
+     */
+    MOV("6D6F6F76"),
+
+    /**
+     * Windows Media.
+     */
+    ASF("3026B2758E66CF11"),
+
+    GZ("1F8B08"),
+
+    /**
+     * MIDI.
+     */
+    MID("4D546864");
+
+    private final String header;
+
+    FileType(String header) {
+        this.header = header;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    // 静态方法：根据序号获取枚举常量
+
+    /**
+     * 根据文件头获取文件类型。
+     *
+     * @param header 文件头。
+     * @return 文件类型。
+     */
+    public static FileType parseHeader(String header) {
+        FileType[] items = values();
+        // 遍历所有常量
+        for (FileType item : items) {
+            // 如果某个常量的序号与传入参数相同，则返回该常量并终止循环。
+            if (item.header.equals(header)) {
+                return item;
+            }
+        }
+
+        // 如果传入参数未匹配到任何常量，则返回空值。
+        return null;
+    }
+}
--- M01_Overview/C00_Temporary/src/main/java/HelloWorld.java
+++ M01_Overview/C00_Temporary/src/main/java/HelloWorld.java
@@ -1,13 +1,74 @@
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.InputStream;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        // try {
-            // ObjectOutputStream oos = new ObjectOutputStream();
-        // } catch (IOException e) {
-        //     throw new RuntimeException(e);
-        // }
+    public static void main(String[] args) throws IOException {
+        FileType fileType = getFileType("/home/bi4vmr/Picture/测试图片/P01.jpg");
+        System.out.println(fileType);
+    }
+
+    // 获取文件类型
+    public static FileType getFileType(String filePath) throws IOException {
+        String header = getFileHeader(filePath);
+        if (header == null || header.isEmpty()) {
+            return null;
+        }
+
+        header = header.toUpperCase();
+        FileType[] fileTypes = FileType.values();
+        for (FileType type : fileTypes) {
+            if (header.startsWith(type.getHeader())) {
+                return type;
+            }
+        }
+
+        return null;
+    }
+
+    // 获取文件头
+    private static String getFileHeader(String filePath) throws IOException {
+        InputStream inputStream = null;
+        byte[] buffer = new byte[32];
+        try {
+            inputStream = new FileInputStream(filePath);
+            int count = inputStream.read(buffer, 0, 20);
+            if (count <= 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return bytesToHexText(buffer);
+    }
+
+    // 将二进制数据转为16进制文本
+    private static String bytesToHexText(byte[] src) {
+        if (src == null || src.length == 0) {
+            return null;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : src) {
+            // 只保留低8位，然后将其转为16进制文本。
+            String hex = Integer.toHexString(b & 0xFF);
+            // 如果本字节对应的16进制文本只有一个字符，则在前面补"0"。
+            if (hex.length() < 2) {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append(hex);
+        }
+
+        return stringBuilder.toString();
     }
 }
