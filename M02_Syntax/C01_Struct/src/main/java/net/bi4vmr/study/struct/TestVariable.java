c907d5e00e66e9bb53e90f28fab198e6d49fcf88
BI4VMR <bi4vmr@outlook.com>
2024-08-05 13:42:27 +0800

    AF46BEBB-8EE2-4182-A2FB-D9DB52DA1FC6

--- M02_Syntax/C01_Struct/src/main/java/net/bi4vmr/study/struct/TestVariable.java
+++ M02_Syntax/C01_Struct/src/main/java/net/bi4vmr/study/struct/TestVariable.java
@@ -12,7 +12,9 @@ public class TestVariable {
     }
 
     /**
-     * 示例：变量。
+     * 示例：变量的基本应用。
+     * <p>
+     * 在本示例中，我们定义一些变量，并访问它们。
      */
     static void example01() {
         // 声明变量“姓名”
@@ -26,7 +28,9 @@ public class TestVariable {
     }
 
     /**
-     * 示例：常量。
+     * 示例：常量的基本应用。
+     * <p>
+     * 在本示例中，我们定义一些常量，并访问它们。
      */
     static void example02() {
         // 示例：常量"PI"
@@ -34,5 +38,23 @@ public class TestVariable {
 
         // 访问常量：将常量的值输出到控制台
         System.out.print("圆周率：" + PI);
+    }
+
+    /**
+     * 示例：变量的作用域。
+     * <p>
+     * 在本示例中，我们定义一些变量，并在不同的作用域中访问它们。
+     */
+    static void example04() {
+        int i = 1;
+
+        // 定义一个“块”
+        {
+            int x = 5;
+            // 块的内部可以访问自己的变量与父级块的变量
+            System.out.println("i:" + i + "; x:" + x);
+        }
+        // 块的外部不能访问块内部的变量
+        System.out.println("i:" + i + "; x:can not be accessed!");
     }
 }
