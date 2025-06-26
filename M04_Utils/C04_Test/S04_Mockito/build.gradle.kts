1176039d70d266363e3c70457b1d53b57f940c2e
BI4VMR <bi4vmr@outlook.com>
2025-06-26 18:10:44 +0800

    219A942E-47AD-4AE3-97FF-32D9EF424978

--- M04_Utils/C04_Test/S04_Mockito/build.gradle.kts
+++ M04_Utils/C04_Test/S04_Mockito/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("jacoco")
 }
 
 tasks.withType<JavaCompile> {
@@ -7,6 +8,47 @@ tasks.withType<JavaCompile> {
 }
 
 dependencies {
+    implementation(libJava.gson)
+
     testImplementation(libJava.junit4)
     testImplementation(libJava.mockito.core)
 }
+
+// Jacoco配置
+jacoco {
+    // 指定Jacoco版本
+    toolVersion = "0.8.13"
+    // 指定报告输出目录
+    reportsDirectory = layout.buildDirectory.dir("jacocoReports")
+}
+
+// 在Gradle Test任务执行后自动执行Jacoco任务
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    // 设置排除项
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude(
+                        "net/**",
+                    )
+                }
+            }
+        )
+    )
+}
+
+// tasks.jacocoTestReport {
+//     reports {
+//         xml.required = false
+//         csv.required = false
+//         // 指定HTML报告输出路径（覆盖jacoco -> reportsDirectory）
+//         html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+//     }
+// }
