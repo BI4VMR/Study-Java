93e8d398226d12f8a5f03bb5da5a760f17df6612
BI4VMR <bi4vmr@outlook.com>
2025-07-12 17:10:54 +0800

    AB9F656D-6108-4260-AE56-59A911E26E2F

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,126 +4,23 @@
 pluginManagement {
     // 声明Gradle插件仓库
     repositories {
-        // 添加本地私有仓库与代理镜像，无法直连时应当禁用该配置。
-        val hostName: String = java.net.InetAddress.getLocalHost().hostName
-        println("Current host name is [$hostName]")
-        var isInPrivateLAN = false
-        run {
-            java.net.NetworkInterface.getNetworkInterfaces().toList().forEach {
-                it.inetAddresses.toList().forEach { addr ->
-                    if ((addr is java.net.Inet4Address) && (addr.hostAddress.startsWith("172.16."))) {
-                        isInPrivateLAN = true
-                        return@run
-                    }
-                }
-            }
-        }
-        println("Current host in private LAN? [$isInPrivateLAN]")
-
-        if (false && hostName.startsWith("BI4VMR") && isInPrivateLAN) {
-            println("Current host is in private network, add LAN repositorys.")
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-mirror-tencent/")
-            }
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-private/")
-            }
-        } else {
-            // VPN is not available now.
-            // if (java.net.InetAddress.getByName("192.168.128.1").isReachable(2000)) {
-            //     println("Current host is not in private network, add VPN repositorys.")
-            //     maven {
-            //         isAllowInsecureProtocol = true
-            //         setUrl("http://192.168.128.1:8081/repository/maven-mirror-tencent/")
-            //     }
-            //     maven {
-            //         isAllowInsecureProtocol = true
-            //         setUrl("http://192.168.128.1:8081/repository/maven-private/")
-            //     }
-            // } else {
-            //     println("Current host is not in private network, add LOCAL repositorys.")
-            mavenLocal()
-            // }
-        }
-
-        // 腾讯云仓库镜像：Maven中心仓库+Google+JCenter
-        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        // 阿里云仓库镜像：Maven中心仓库+JCenter
-        maven { setUrl("https://maven.aliyun.com/repository/public/") }
         // 阿里云仓库镜像：Gradle社区插件
         maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
+        // 阿里云仓库镜像：Maven中心仓库+JCenter
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        // 阿里云仓库镜像：Google仓库
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
+        // 腾讯云仓库镜像：Maven中心仓库+Google+JCenter
+        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
 
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
+        google()
     }
 }
 
 // 所有模块的依赖配置
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    // 声明Maven组件仓库
-    repositories {
-        // 添加本地私有仓库与代理镜像，无法直连时应当禁用该配置。
-        val hostName: String = java.net.InetAddress.getLocalHost().hostName
-        var isInPrivateLAN = false
-        run {
-            java.net.NetworkInterface.getNetworkInterfaces().toList().forEach {
-                it.inetAddresses.toList().forEach { addr ->
-                    if ((addr is java.net.Inet4Address) && (addr.hostAddress.startsWith("172.16."))) {
-                        isInPrivateLAN = true
-                        return@run
-                    }
-                }
-            }
-        }
-
-        if (false && hostName.startsWith("BI4VMR") && isInPrivateLAN) {
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-mirror-tencent/")
-            }
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-jitpack/")
-            }
-            maven {
-                isAllowInsecureProtocol = true
-                setUrl("http://172.16.5.1:8081/repository/maven-private/")
-            }
-        } else {
-            // VPN is not available now.
-            // if (java.net.InetAddress.getByName("192.168.128.1").isReachable(2000)) {
-            //     maven {
-            //         isAllowInsecureProtocol = true
-            //         setUrl("http://192.168.128.1:8081/repository/maven-mirror-tencent/")
-            //     }
-            //     maven {
-            //         isAllowInsecureProtocol = true
-            //         setUrl("http://192.168.128.1:8081/repository/maven-jitpack/")
-            //     }
-            //     maven {
-            //         isAllowInsecureProtocol = true
-            //         setUrl("http://192.168.128.1:8081/repository/maven-private/")
-            //     }
-            // } else {
-            mavenLocal()
-            // }
-        }
-
-        // 腾讯云仓库镜像：Maven中心仓库+Google+JCenter
-        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        // 阿里云仓库镜像：Maven中心仓库+JCenter
-        maven { setUrl("https://maven.aliyun.com/repository/public/") }
-        // 阿里云仓库镜像：Google
-        maven { setUrl("https://maven.aliyun.com/repository/google/") }
-
-        mavenCentral()
-        google()
-        maven { setUrl("https://jitpack.io/") }
-    }
-
     // 版本管理配置
     versionCatalogs {
         // 公共组件(Java)
@@ -141,7 +38,8 @@ dependencyResolutionManagement {
 /* ----- 工程结构声明 ----- */
 // 主工程名称
 rootProject.name = "Study-Java"
-
+// 加载自定义插件
+includeBuild("plugin")
 
 // ----- 基础知识 -----
 include(":M01_Overview:C00_Temporary")
@@ -180,8 +78,8 @@ include(":M04_Utils:C02_DateTime")
 include(":M04_Utils:C03_Math")
 include(":M04_Utils:C04_Test:S01_Base")
 include(":M04_Utils:C04_Test:S02_JUnit4")
-include(":M04_Utils:C04_Test:S03_JUnit5")
-include(":M04_Utils:C04_Test:S04_Mockito")
+include(":M04_Utils:C04_Test:S02_JUnit5")
+include(":M04_Utils:C04_Test:S03_Mockito")
 
 // ----- 数据存储 -----
 include(":M05_Storage:C01_File:S01_Base")
