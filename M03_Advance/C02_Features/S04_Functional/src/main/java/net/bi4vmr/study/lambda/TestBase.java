f84b01db454f1fb4e4b5bbe06f027313f21715c7
BI4VMR <bi4vmr@outlook.com>
2026-05-07 17:56:29 +0800

    7665A2F0-8033-4492-9CB0-80C15958E445

---
+++ M03_Advance/C02_Features/S04_Functional/src/main/java/net/bi4vmr/study/lambda/TestBase.java
@@ -0,0 +1,24 @@
+package net.bi4vmr.study.lambda;
+
+/**
+ * 示例代码：基本应用。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
+public class TestBase {
+
+    public static void main(String[] args) {
+        // example03();
+    }
+
+
+    /**
+     * 示例一：表示二维坐标。
+     * <p>
+     * 在本示例中，我们定义一个类表示二维坐标，允许调用者传入整数、小数等多种形式的原始数据。
+     */
+    static void example01() {
+
+    }
+}
