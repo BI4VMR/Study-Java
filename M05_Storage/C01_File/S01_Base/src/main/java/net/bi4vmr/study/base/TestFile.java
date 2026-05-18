79e85164f0e8c49d2357ecb62c7cb8234a88f283
BI4VMR <bi4vmr@outlook.com>
2026-05-18 17:57:44 +0800

    CC11B11D-298D-4F8E-ABC2-082AA873E958

--- M05_Storage/C01_File/S01_Base/src/main/java/net/bi4vmr/study/base/TestFile.java
+++ M05_Storage/C01_File/S01_Base/src/main/java/net/bi4vmr/study/base/TestFile.java
@@ -13,75 +13,54 @@ import java.io.IOException;
 public class TestFile {
 
     public static void main(String[] args) throws Exception {
-        example09();
+        example02();
     }
+
 
     /**
      * 示例一：获取文件的属性。
      * <p>
-     * 在本示例中，我们演示File类的常用方法。
+     * 在本示例中，我们通过File实例获取文件的属性。
      */
     static void example01() throws IOException {
-        String path = "dir" + File.separator + "test.txt";
-        File file1 = new File(path);
-        System.out.println("文件名称：" + file1.getName());
-        System.out.println("路径：" + file1.getPath());
-        System.out.println("绝对路径：" + file1.getAbsolutePath());
-        System.out.println("绝对路径（标准）：" + file1.getCanonicalPath());
-        System.out.println("文件大小：" + file1.length());
-        System.out.println("最后修改时间：" + file1.lastModified());
-        System.out.println("父级目录：" + file1.getParent());
-        System.out.println("是否存在：" + file1.exists());
-        System.out.println("是否为文件：" + file1.isFile());
-        System.out.println("是否为目录：" + file1.isDirectory());
-        System.out.println("是否隐藏：" + file1.isHidden());
-        System.out.println("是否可读：" + file1.canRead());
-        System.out.println("是否可写：" + file1.canWrite());
-        System.out.println("是否可执行：" + file1.canExecute());
+        String path = "M05_Storage/C01_File/S01_Base/src/main/resources/test.txt";
+        File file = new File(path);
+        System.out.println("文件名称：" + file.getName());
+        System.out.println("路径：" + file.getPath());
+        System.out.println("绝对路径：" + file.getAbsolutePath());
+        System.out.println("绝对路径（标准）：" + file.getCanonicalPath());
+        System.out.println("文件大小：" + file.length());
+        System.out.println("最后修改时间：" + file.lastModified());
+        System.out.println("父级目录：" + file.getParent());
+        System.out.println("是否存在：" + file.exists());
+        System.out.println("是否为文件：" + file.isFile());
+        System.out.println("是否为目录：" + file.isDirectory());
+        System.out.println("是否隐藏：" + file.isHidden());
+        System.out.println("是否可读：" + file.canRead());
+        System.out.println("是否可写：" + file.canWrite());
+        System.out.println("是否可执行：" + file.canExecute());
     }
+
 
     /**
-     * 示例二：创建文件。
+     * 示例二：创建目录与文件。
      * <p>
-     * 在本示例中，我们演示File类的常用方法。
+     * 在本示例中，我们在当前模块的资源目录中创建名为 `test` 的目录，并在其中创建一个内容为空的文件。
      */
     static void example02() throws IOException {
-        File file = new File("1.txt");
-        // 文件不存在时进行创建操作
-        if (!file.exists()) {
-            boolean result = file.createNewFile();
-            System.out.println("文件创建结果：" + result);
-        } else {
-            System.out.println("文件已存在！");
-        }
-    }
+        String prefix = "M05_Storage/C01_File/S01_Base/src/main/resources/";
 
-    /*
-     * 文件管理 - 创建目录
-     */
-    static void example03() {
-        File file = new File("test");
-        // 目录不存在时进行创建操作
-        if (!file.exists()) {
-            boolean result = file.mkdir();
-            System.out.println("目录创建结果：" + result);
-        } else {
-            System.out.println("目录已存在！");
-        }
-    }
+        // 目录路径
+        File dirPath = new File(prefix, "test");
+        // 目录已存在或无写入权限等情况将返回 `false` 。
+        boolean result = dirPath.mkdir();
+        System.out.println("目录创建结果：" + result);
 
-    /*
-     * 文件管理 - 删除文件
-     */
-    static void example04() {
-        File file = new File("1.txt");
-        // 文件存在时进行删除操作
-        if (file.exists()) {
-            boolean result = file.delete();
-            System.out.println("文件删除结果：" + result);
-        } else {
-            System.out.println("文件不存在，无需删除！");
-        }
+        // 文件路径
+        File filePath = new File(dirPath.getPath(), "1.txt");
+        // 文件已存在或无写入权限等情况将返回 `false` 。
+        boolean result2 = filePath.createNewFile();
+        System.out.println("文件创建结果：" + result2);
     }
 
     /*
@@ -100,6 +79,20 @@ public class TestFile {
             System.out.println("文件移动结果：" + result);
         } else {
             System.out.println("源文件不存在或目标文件已存在！");
+        }
+    }
+
+    /*
+     * 文件管理 - 删除文件
+     */
+    static void example04() {
+        File file = new File("1.txt");
+        // 文件存在时进行删除操作
+        if (file.exists()) {
+            boolean result = file.delete();
+            System.out.println("文件删除结果：" + result);
+        } else {
+            System.out.println("文件不存在，无需删除！");
         }
     }
 
@@ -210,5 +203,25 @@ public class TestFile {
             System.out.printf("使用率：%.2f%%", useRate);
             System.out.println();
         }
+    }
+
+
+    /**
+     * 示例十：获取常用路径。
+     * <p>
+     * 在本示例中，我们通过系统属性获取常用目录的路径。
+     */
+    static void example10() {
+        // 终端工作目录
+        String workDir = System.getProperty("user.dir");
+        System.out.println("终端工作目录：" + workDir);
+
+        // 当前用户目录
+        String userDir = System.getProperty("user.home");
+        System.out.println("当前用户目录：" + userDir);
+
+        // 临时目录
+        String tempDir = System.getProperty("java.io.tmpdir");
+        System.out.println("临时目录：" + tempDir);
     }
 }
